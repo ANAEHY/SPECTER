ea7b777c0e3bdbad0de9a2aa8e0897328791eba6
SPECTR <52015525+ANAEHY@users.noreply.github.com>
2026-03-06 15:33:03 +0300

    Update update_keys.py

--- update_keys.py
+++ update_keys.py
@@ -8,7 +8,7 @@ from urllib.parse import urlparse, urlunparse, quote, unquote
 
 # ===== GITHUB НАСТРОЙКИ =====
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')   # секрет в GitHub Actions
-GITHUB_REPO  = 'spermoeshka/NEW_SRECTER'   # твой репозиторий
+GITHUB_REPO  = 'ANAEHY/SPECTER'
 GITHUB_FILE  = 'keys.txt'                  # имя файла (латиницей!)
 GITHUB_BRANCH = 'main'
 
